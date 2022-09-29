78b0d68a0d8289f9c9126dc4c6d286e6973ad14b
Shadhin Ahmed <shadhinplanet@gmail.com>
2022-09-30 00:10:52 +0600

    Init Migrations

---
+++ database/migrations/2022_09_29_180204_create_lessons_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lessons', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('slug');
+            $table->longText('content');
+            $table->enum('status', ['active','inactive'])->default('active');
+            $table->foreignId('course_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lessons');
+    }
+};
